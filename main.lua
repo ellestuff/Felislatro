c9ab41ddd871496674e32776de6ca7cc74aff176
ellestuff. <ellef1566@gmail.com>
2025-11-11 16:37:30 +0000

    cryptid scaling

--- main.lua
+++ main.lua
@@ -21,7 +21,8 @@ SMODS.Atlas {
 
 felis_sophie_scaling = {
 	function(mult,mod) return math.floor(math.sqrt(to_number(mult)/(100*mod))) end,
-	function(mult,mod) return to_number(mult/mod)<1 and 0 or math.floor(math.sqrt(to_number(math.log(mult/mod)/2))) end
+	function(mult,mod) return to_number(mult/mod)<1 and 0 or math.floor(math.sqrt(to_number(math.log(mult/mod)/2))) end,
+	function(mult,mod) return to_number(mult/mod)<1 and 0 or math.floor(math.sqrt(to_number(math.log(math.log(mult/mod))))) end
 }
 
 -- Add sophie stuff
@@ -84,7 +85,7 @@ SMODS.current_mod.config_tab = function()
 		}),
 		create_option_cycle({opt_callback = "conf_felis_soph_scaling",
 			label = "Sophie Scaling",
-			options = {"Vanilla - sqrt(mult/100)", "Extreme - sqrt(log(mult)/2)"},
+			options = {"Vanilla - sqrt(mult/100)", "Extreme - sqrt(log(mult)/2)", "Cryptid - sqrt(log(log(mult)))"},
 			current_option = config.sophie.scaling,
 			scale = 0.8,
 			info = {
